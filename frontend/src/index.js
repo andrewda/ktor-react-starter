0e1d302f635df572542f8b9ec1528071f0d0ed88
Andrew Dassonville <dassonville.andrew@gmail.com>
2019-04-19 14:15:07 -0700

    Add example routing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,74 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Link, Route} from "react-router-dom";
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+function App() {
+    return (
+        <Router>
+            <div>
+                <Header />
+
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={About} />
+                <Route path="/topics" component={Topics} />
+            </div>
+        </Router>
+    );
+}
+
+function Home() {
+    return <h2>Home</h2>;
+}
+
+function About() {
+    return <h2>About</h2>;
+}
+
+function Topic({ match }) {
+    return <h3>Requested Param: {match.params.id}</h3>;
+}
+
+function Topics({ match }) {
+    return (
+        <div>
+            <h2>Topics</h2>
+
+            <ul>
+                <li>
+                    <Link to={`${match.url}/components`}>Components</Link>
+                </li>
+                <li>
+                    <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+                </li>
+            </ul>
+
+            <Route path={`${match.path}/:id`} component={Topic} />
+            <Route
+                exact
+                path={match.path}
+                render={() => <h3>Please select a topic.</h3>}
+            />
+        </div>
+    );
+}
+
+function Header() {
+    return (
+        <ul>
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/about">About</Link>
+            </li>
+            <li>
+                <Link to="/topics">Topics</Link>
+            </li>
+        </ul>
+    );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
